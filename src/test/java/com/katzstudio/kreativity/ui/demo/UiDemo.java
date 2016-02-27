9b4e764d29b6b808345edc0bc14522b1225dd9d3
Alexandru Pana <astronothing@gmail.com>
2016-02-27 14:25:50 +0200

    Enable support for blending in UiDemo

--- src/test/java/com/katzstudio/kreativity/ui/demo/UiDemo.java
+++ src/test/java/com/katzstudio/kreativity/ui/demo/UiDemo.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -22,6 +21,13 @@ import com.katzstudio.kreativity.ui.component.KrCollapsiblePanel;
 import com.katzstudio.kreativity.ui.component.KrLabel;
 import com.katzstudio.kreativity.ui.component.KrScrollPanel;
 import com.katzstudio.kreativity.ui.component.KrTextField;
+
+import static com.badlogic.gdx.Gdx.gl;
+import static com.badlogic.gdx.graphics.GL20.GL_BLEND;
+import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
+import static com.badlogic.gdx.graphics.GL20.GL_DEPTH_BUFFER_BIT;
+import static com.badlogic.gdx.graphics.GL20.GL_ONE_MINUS_SRC_ALPHA;
+import static com.badlogic.gdx.graphics.GL20.GL_SRC_ALPHA;
 
 /**
  */
@@ -49,12 +55,16 @@ public class UiDemo extends Game {
 
     @Override
     public void create() {
+        gl.glEnable(GL_BLEND);
+        gl.glDepthMask(true);
+        gl.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
         spriteBatch = new SpriteBatch();
 
         WIDGET_BACKGROUND = KrToolkit.createColorDrawable(KrColor.rgb(0x252525));
         RED_DRAWABLE = KrToolkit.createColorDrawable(Color.RED);
 
-        Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 1);
+        gl.glClearColor(0.3f, 0.3f, 0.3f, 1);
 
         KreativitySkin.instance().install();
         canvas = new KrCanvas();
@@ -157,8 +167,7 @@ public class UiDemo extends Game {
 
     @Override
     public void render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         canvas.update(Gdx.graphics.getDeltaTime());
         canvas.draw();
     }
