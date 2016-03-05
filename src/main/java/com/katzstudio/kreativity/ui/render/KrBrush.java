4fdd9557a4d6f40805c1b11fb209159e8838995f
Alexandru Pana <astronothing@gmail.com>
2016-03-05 15:47:24 +0200

    Fix KrButton rendering tests

--- src/main/java/com/katzstudio/kreativity/ui/render/KrBrush.java
+++ src/main/java/com/katzstudio/kreativity/ui/render/KrBrush.java
@@ -1,10 +1,13 @@
 package com.katzstudio.kreativity.ui.render;
 
+import lombok.EqualsAndHashCode;
+
 /**
  * The {@link KrBrush} class describes the fill pattern of shapes rendered by the {@link KrRenderer}
  * <p>
  * Due to the nature of the {@link KrRenderer} implementation, the {@link KrBrush} cannot offer any interface.
  * Instead, subclasses offer the data necessary for the renderer to perform the actual rendering.
  */
+@EqualsAndHashCode
 public class KrBrush {
 }
