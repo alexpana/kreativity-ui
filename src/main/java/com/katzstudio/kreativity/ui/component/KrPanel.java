4c063a863c1d24c2aa112439c7edef5213b383d4
Alexandru Pana <astronothing@gmail.com>
2016-04-13 21:05:41 +0300

    Cleanup KrPanel

--- src/main/java/com/katzstudio/kreativity/ui/component/KrPanel.java
+++ src/main/java/com/katzstudio/kreativity/ui/component/KrPanel.java
@@ -1,14 +1,12 @@
 package com.katzstudio.kreativity.ui.component;
 
-import com.badlogic.gdx.math.Vector2;
 import com.katzstudio.kreativity.ui.layout.KrAbsoluteLayout;
 import com.katzstudio.kreativity.ui.layout.KrLayout;
-import com.katzstudio.kreativity.ui.render.KrRenderer;
 
 import static com.katzstudio.kreativity.ui.KrToolkit.getDefaultToolkit;
 
 /**
- * A simple panel that can host other components. It can be styled with a checkboxBackground.
+ * A simple panel that can host other components.
  */
 public class KrPanel extends KrWidget {
 
@@ -20,16 +18,4 @@ public class KrPanel extends KrWidget {
         setDefaultStyle(getDefaultToolkit().getSkin().getStyle(KrPanel.class));
         setLayout(layout);
     }
-
-    @Override
-    protected void drawSelf(KrRenderer renderer) {
-        renderer.setBrush(getStyle().background);
-        renderer.fillRect(0, 0, getWidth(), getHeight());
-    }
-
-    @Override
-    public Vector2 calculatePreferredSize() {
-        return getLayout().getPreferredSize();
-    }
-
 }
